536cfb133d3489463ae03931ce74f6ffbeb6145e
Frank Carag <cfrankluis@gmail.com>
2022-05-09 07:55:42 -0400

    Project 2 Scaffold

---
+++ project2WS/socialnetwork/src/main/java/application/model/User.java
@@ -0,0 +1,46 @@
+package application.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="user_table")
+public class User {
+	
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name", nullable=false)
+	private String firstName;
+	
+	@Column(name="last_name", nullable=false)
+	private String lastName;
+	
+	@Column(name="email", nullable=false)
+	private String email;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+
+	public User(String firstName, String lastName, String email, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+}
