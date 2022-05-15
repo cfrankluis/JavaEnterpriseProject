798614d9e4ec58a58905da09e7ac7585dfa62b5b
Maplesushi <58122941+Maplesushi@users.noreply.github.com>
2022-05-15 17:27:15 -0400

    Delete project2WS directory

--- project2WS/socialnetwork/src/main/java/application/dao/CommentDao.java
+++
@@ -1,9 +0,0 @@
-package application.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import application.model.Comment;
-
-public interface CommentDao extends JpaRepository<Comment, Integer> {
-
-}
