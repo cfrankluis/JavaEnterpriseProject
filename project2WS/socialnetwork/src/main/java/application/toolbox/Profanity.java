a2309bd5f78d03c7251f5afa41da8e21abffa2a9
Dillon Meier <meier.dillon.95@gmail.com>
2022-05-15 20:28:14 -0400

    bye bye thymeleaf

--- project2WS/socialnetwork/src/main/java/application/toolbox/Profanity.java
+++ project2WS/socialnetwork/src/main/java/application/toolbox/Profanity.java
@@ -1,86 +1,74 @@
 package application.toolbox;
 
 import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 public class Profanity {
-	public static ArrayList<String> badWords() {
-
+	public static  ArrayList<String> badWords (){
+		
 		ArrayList<String> badWord = new ArrayList<String>();
-
+		
 		badWord.add("barnacle");
 		badWord.add("fluff");
+		badWord.add("crapbiscuits");
 		badWord.add("balderdash");
+		badWord.add("corncuts");
+		badWord.add("sonofamonkey");
+		badWord.add("dagnabbit");
+		badWord.add("pooonastick");
+		badWord.add("sufferin’succotash");
+		badWord.add("oh,snap");
+		badWord.add("cheeseandcrackers");
 		badWord.add("frack");
+		badWord.add("shitakemushrooms");
+		badWord.add("motherofpearl");
 		badWord.add("jeepers");
+		badWord.add("holyguacamole");
 		badWord.add("drat");
 		badWord.add("cuss");
 		
-		badWord.add("Barnacle");
-		badWord.add("Fluff");
-		badWord.add("Balderdash");
-		badWord.add("Frack");
-		badWord.add("Jeepers");
-		badWord.add("Drat");
-		badWord.add("Cuss");
-
 		return badWord;
 	}
 
-	/**
-	 * Returns a boolean if there is profanity in the string even if you use leek speak.
-	 * @param str
-	 * @return
-	 * @author Gibbons
-	 */
 	public static boolean doesNotHaveProfanity(String str) {
 		ArrayList<String> badWord = badWords();
+		
+		//1337 5P34K AKA leet speak
+		str = str.replaceAll("1","i");
+        str = str.replaceAll("!","i");
+        str = str.replaceAll("3","e");
+        str = str.replaceAll("4","a");
+        str = str.replaceAll("@","a");
+        str = str.replaceAll("5","s");
+        str = str.replaceAll("7","t");
+        str = str.replaceAll("0","o");
+        str = str.replaceAll("9","g");
+        str = str.replaceAll("5","s");
+        
+        //remove spacing
+        str = str.replaceAll(" ", "");
+        str = str.replaceAll("/", "");
+        str = str.replaceAll("-", "");
+        str = str.replaceAll("_", "");
+        str = str.replaceAll(".", "");
+        str = str.replaceAll(",", "");
 
-		// 1337 5P34K AKA leet speak
-		str = str.replaceAll("1", "i");
-		str = str.replaceAll("!", "i");
-		str = str.replaceAll("3", "e");
-		str = str.replaceAll("4", "a");
-		str = str.replaceAll("@", "a");
-		str = str.replaceAll("5", "s");
-		str = str.replaceAll("7", "t");
-		str = str.replaceAll("0", "o");
-		str = str.replaceAll("9", "g");
-		str = str.replaceAll("5", "s");
-
-		// remove spacing
-		str = str.replaceAll(" ", "");
-		str = str.replaceAll("/", "");
-		str = str.replaceAll("-", "");
-		str = str.replaceAll("_", "");
-		str = str.replaceAll(".", "");
-		str = str.replaceAll(",", "");
-
-		for (String words : badWord) {
-			if (str.toLowerCase().contains(words)) {
+		
+		
+		for(String words : badWord) {
+			if(str.toLowerCase().contains(words)) {
 				return false;
 			}
 		}
-
+		
 		return true;
 	}
-
-
-	/**
-	 * returns a censored string replacing all profanity with #@$%
-	 * @param str
-	 * @return
-	 * @author Gibbons
-	 */
+	
+/// make a good replacement filter
 	public static String replaceBadWords(String str) {
-		ArrayList<String> badWord = badWords();;
-
-		for (int i = 0; i < badWord.size(); i++) {
-			System.out.println(badWord.get(i));
-
-			str = str.replace(badWord.get(i), "#@$%");
-			System.out.println(str);
-		}
-		return str;
+		String badWord = "cuss";
+		
+		return str = str.replaceAll(badWord, "#@$%");
+		
 	}
+
 }
