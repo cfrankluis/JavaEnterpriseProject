0a1c7fea481ce574a20ee5c2f247e1f52224e365
Dillon Meier <meier.dillon.95@gmail.com>
2022-05-20 23:37:59 -0400

    successfully pulled url param in profile.js, parsing to object to use in an ajax request.

--- project2WS/socialnetwork/src/main/resources/static/js/profilepage.js
+++ project2WS/socialnetwork/src/main/resources/static/js/profilepage.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     const queryString = window.location.search;
-    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const user = urlParams.get('user')
+    console.log(user);
     //friendOrMe();
 }
 
